1a5c7f65d7f94717d66607d02300c7213a7aed36
Elena <velenavl@inbox.ru>
2022-09-28 01:16:55 +0300

    Ex006

---
+++ Ex006/Program.cs
@@ -0,0 +1,15 @@
+﻿// Перебор слов. В некотором машинном алфавите имеются 4 буквы "а", "и", "с" и "в". Покажите все слова, состоящие из Т букв, которые можно построить из букв этого алфавита.
+int n = 1;
+void FindWords(string alphabet, char[] word, int length = 0)
+{
+    if (length == word.Length)
+    {
+        Console.WriteLine($"{n++} {new String(word)}"); return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[length] = alphabet[i];
+        FindWords(alphabet, word, length + 1);
+    }
+}
+FindWords("аисв", new char[2]);
