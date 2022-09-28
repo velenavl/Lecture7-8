249545a08e6efb9127123d47ea7f483b54ff056b
Elena <velenavl@inbox.ru>
2022-09-28 23:16:35 +0300

    Ex007

---
+++ Ex007/Program.cs
@@ -0,0 +1,21 @@
+﻿// Посмотреть содержимое папок (директории)
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + " ");
+    }
+
+    FileInfo[] files = catalog.GetFiles();
+
+    for (int i = 0; i < files.Length; i++)
+    {
+      Console.WriteLine($"{indent}{files[i].Name}");  
+    }
+}
+ string path = @"C:\ ";
+ CatalogInfo(path);
